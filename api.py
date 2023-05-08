53d61774dcee2b5a29d652352dd358968b2bb2b5
S-Vijay-vj <112348575+S-Vijay-vj@users.noreply.github.com>
2023-05-08 23:16:39 +0530

    added api files

---
+++ api.py
@@ -0,0 +1,21 @@
+# Importing the libraries
+from fastapi import FastAPI
+import csv
+
+# Creating a fastapi instance
+app = FastAPI()
+
+
+# path operation decorator
+@app.get("/data")
+# path operation function
+async def read_data():
+    # UTF-8 is commonly used for text files, web pages, and APIs that handle multilingual text data.
+    with open(
+        "D:\\python\\webscraping\\Flipkart_web_scraping\\flipkart_laptop_scraping.csv",
+        newline="",
+        encoding="utf-8",
+    ) as csvfile:
+        data = list(csv.DictReader(csvfile))
+        return data
+    # This opens the csv file and read the contents return the data as list of Dictionaries
